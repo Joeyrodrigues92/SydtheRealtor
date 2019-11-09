16e9c4d9e8901ffbdcb7d100e68d72cabb770e00
Joey Rodrigues <joeyrodrigues@Joeys-MBP.home>
2019-11-08 22:03:41 -0500

    email test

--- views/contact.php
+++ views/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+    mail("joeyrodrigues2@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
 
 ?><!DOCTYPE <!DOCTYPE html>
